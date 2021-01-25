2d10a5641384395c4341b5fffef9a4fcb8fbaa5d
Shane R <imspammails@gmail.com>
2021-01-25 19:43:31 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,7 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  kids_name = ""
-  
+  birthday_kids.each do |name, age|
+    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  end
 end
